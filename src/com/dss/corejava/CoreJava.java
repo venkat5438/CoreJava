b378f34333b936d3595adcb1e8deeb85e57e368f
venkat5438 <venkatramreddy108@gmail.com>
2017-02-13 12:35:40 +0530

    file modified

--- src/com/dss/corejava/CoreJava.java
+++ src/com/dss/corejava/CoreJava.java
@@ -2,8 +2,15 @@ package com.dss.corejava;
 
 public class CoreJava {
 
+	public static int a;
+	public int b;
+	
 	public static void main(String[] args) {
 		System.out.println("venkat");// printing a statement
+		CoreJava c=new CoreJava();
+		
+		System.out.println(a);
+		System.out.println(c.b);
 	}
 
 }
